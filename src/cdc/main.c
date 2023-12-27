d105ecba28e029bec34ac22cb3d576d31f76bb7c
Nicola Strappazzon C <nstrappazzonc@gmail.com>
2023-12-27 20:25:45 +0100

    fix - own ch552.h

--- src/cdc/main.c
+++ src/cdc/main.c
@@ -2,29 +2,21 @@
 #include "ch552.h"
 #include "utils.h"
 
-#define bUART0_PIN_X      0x10      // UART0 alternate pin enable: 0=RXD0/TXD0 on P3.0/P3.1, 1=RXD0/TXD0 on P1.2/P1.3
-#define bT1_GATE          0x80      // gate control of timer1: 0=timer1 run enable while TR1=1, 1=timer1 run enable while P3.3 (INT1) pin is high and TR1=1
-#define bT1_CT            0x40      // counter or timer mode selection for timer1: 0=timer, use internal clock, 1=counter, use P3.5 (T1) pin falling edge as clock
-#define bT1_M1            0x20      // timer1 mode high bit
-#define MASK_T1_MOD       0x30      // bit mask of timer1 mode
-#define bTMR_CLK          0x80      // fastest internal clock mode for timer 0/1/2 under faster clock mode: 0=use divided clock, 1=use original Fsys as clock without dividing
-#define bT1_CLK           0x20      // timer1 internal clock frequency selection: 0=standard clock, Fsys/12, 1=faster clock, Fsys/4 if bTMR_CLK=0 or Fsys if bTMR_CLK=1
-#define SMOD              0x80
-
-// #define FREQ_SYS 24000000
 #define UART0_BAUDRATE 9600
 
 void UART0Init();
-void UART0Send(uint8_t);
-void UART0SendString(char*);
+uint8_t UART0Read();
+void UART0Write(uint8_t);
+void UART0WriteString(char*);
 
 void main(void) {
     init();
 
     UART0Init();
-
+    UART0WriteString("Hello from CH552T with CH340C\n\r");
+    UART0WriteString("Type any char to reply:\n\r");
     while(1){
-        UART0SendString("Hello from CH552T with CH340C\n\r");
+        UART0Write(UART0Read());
     }
 }
 
@@ -42,21 +34,25 @@ void UART0Init(){
     TR1 = 1; // Timer1 start
     TI = 0;
     REN = 1; // UART0 Receive enable
-    // ES = 1;  // Interrupt enable
-    // EA = 1;
 }
 
-inline void UART0Send(uint8_t byte) __critical{
+void UART0Write(uint8_t byte) {
     SBUF = byte;
-    while(TI ==0); // Wait for done
+    while(TI ==0);
     TI = 0;
 
     return;
 }
 
-void UART0SendString(char* string){
+uint8_t UART0Read() {
+    while(RI == 0);
+    RI = 0;
+    return SBUF;
+}
+
+void UART0WriteString(char* string) {
     while(*string!='\0'){
-        UART0Send(*string);
+        UART0Write(*string);
         string++;
     }
 
