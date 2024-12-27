7d4e815d43ddf9c4e26bc4f1be3849a11856cada
Nicola Strappazzon <nicola@strappazzon.me>
2024-12-27 11:46:03 +0100

    Save changes at: 1735296363

--- inc/data_flash.h
+++
@@ -1,4 +0,0 @@
-#include <stdint.h>
-
-uint8_t WriteDataFlash(uint8_t addr, unsigned char *buf, uint8_t len);
-uint8_t ReadDataFlash(uint8_t addr, uint8_t len, unsigned char *buf);
--- src/data_flash/main.c
+++ src/data_flash/main.c
@@ -1,34 +1,103 @@
-#include "stdio.h"
 #include <ch552.h>
-// #include <data_flash.h>
-// #include <string.h>
 #include <utils.h>
+
+#define RED    P34
+#define GREEN  P33
+#define BLUE   P11
+#define BUTTON P32
+
+uint8_t WriteDataFlash(uint8_t Addr,unsigned char *buf,uint8_t len) {
+    uint8_t i;
+    SAFE_MOD = 0x55;
+    SAFE_MOD = 0xAA;
+    GLOBAL_CFG |= bDATA_WE;
+    SAFE_MOD = 0;
+    ROM_ADDR_H = DATA_FLASH_ADDR >> 8;
+
+    Addr <<= 1;
+    for(i=0;i<len;i++) {
+        ROM_ADDR_L = Addr + i*2;
+        ROM_DATA_L = *(buf+i);
+        if ( ROM_STATUS & bROM_ADDR_OK ) {
+           ROM_CTRL = ROM_CMD_WRITE;
+        }
+        if((ROM_STATUS ^ bROM_ADDR_OK) > 0) return i;
+      }
+    SAFE_MOD = 0x55;
+    SAFE_MOD = 0xAA;
+    GLOBAL_CFG &= ~bDATA_WE;
+    SAFE_MOD = 0;
+    return i;
+}
+
+uint8_t ReadDataFlash(uint8_t Addr,uint8_t len,unsigned char *buf) {
+    uint8_t i;
+    ROM_ADDR_H = DATA_FLASH_ADDR >> 8;
+    Addr <<= 1;
+    for(i=0;i<len;i++) {
+      ROM_ADDR_L = Addr + i*2;
+      ROM_CTRL = ROM_CMD_READ;
+      *(buf+i) = ROM_DATA_L;
+    }
+    return i;
+}
 
 void main(void) {
     init();
 
-    // uint8_t i, m, len;
-    // uint16_t j = 0;
+    uint8_t color;
+
+    ReadDataFlash(1, 1, &color);
+    switch(color) {
+        case 1:
+            RED = 1;
+            break;
+        case 2:
+            GREEN = 1;
+            break;
+        case 3:
+            BLUE = 1;
+            break;
+        default:
+            break;
+    }
+
+    //Set input in pullup mode:
+    P3_MOD_OC |= (1<<BUTTON);
+    P3_DIR_PU |= (1<<BUTTON);
 
     while(1) {
-        // for(int i=0; i<128; i++){
-        //     len = WriteDataFlash(i, &i, 1);
-        //     if(len != 1){
-        //         // printf("Write Err = %02x,m = %02x\n", j, (uint8_t)m);
-        //     }
-        // }
+        if(BUTTON) {
+            delay(50);
 
-        // for(i=0;i<128;i++){
-        //     len = ReadDataFlash(i,1,&m);
-        //     if((len != 1) ||(m != i)){
-        //         // printf("Read Err = %02x, = %02x,addr =%02x ,值= %02x\n",j,(uint8_t)(i*2),(uint8_t)ROM_DATA_L,(uint8_t)m);               
-        //     }
-        // }
-        // // printf("$$OK %02x \n",j);
-        // j++;
+            if(BUTTON) {
+                color++;
+                if (color > 3) {
+                    color = 0;
+                }
 
-        delay(1000);
+                WriteDataFlash(1, &color, 1);
+
+                RED   = 0;
+                GREEN = 0;
+                BLUE  = 0;
+
+                switch(color) {
+                    case 1:
+                        RED = 1;
+                        break;
+                    case 2:
+                        GREEN = 1;
+                        break;
+                    case 3:
+                        BLUE = 1;
+                        break;
+                    default:
+                        break;
+                }
+
+                while(BUTTON);
+            }
+        }
     }
 }
-
-// hacer algo con un led y un pulsador para guardar el estado de la ultima vez que se presiono el boton, conectas y desconecta y carga el ultimo estado.
