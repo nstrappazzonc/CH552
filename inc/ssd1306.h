80c5cedaf560043005ff3bb8105952dff4f11279
Nicola Strappazzon <nicola@strappazzon.me>
2025-01-01 20:41:19 +0100

    Save changes at: 1735760479

--- inc/ssd1306.h
+++ inc/ssd1306.h
@@ -7,7 +7,7 @@
 
 #define OLED_ADDR         0x78 // OLED write address (0x3C << 1).
 #define OLED_COMMAND      0x00 // Set command mode.
-#define OLED_DAT_MODE     0x40 // Set data mode.
+#define OLED_DATA         0x40 // Set data mode.
 #define OLED_MEMORYMODE   0x20 // Set memory addressing mode (following byte).
 #define OLED_STARTLINE    0x40 // Set display start line (0x40-0x7F = 0-63).
 #define OLED_CONTRAST     0x81 // Set display contrast (following byte).
@@ -15,18 +15,19 @@
 #define OLED_XFLIP        0xA1 // Flip display horizontally.
 #define OLED_MULTIPLEX    0xA8 // Set multiplex ratio (following byte).
 #define OLED_DISPLAY_ON   0xAF // Set display on.
-#define OLED_PAGE         0xB0 // Set start page (following byte).
+#define OLED_PAGE         0xB0 // Set start page/line (following byte).
+#define OLED_COLUMN       0x10 // Set start column (following byte).
 #define OLED_YFLIP        0xC8 // Flip display vertically.
 #define OLED_OFFSET       0xD3 // Set display offset (y-scroll: following byte).
 #define OLED_COMPINS      0xDA // Set COM pin config (following byte).
-
-#define SSD1306_SET_COLUMN_ADDR							0x21
-#define SSD1306_SET_PAGE_ADDR							0x22
+#define OLED_WIDTH        128  //
+#define OLED_HEIGHT        64  //
 
 void oled_init(void);
 void oled_clear(void);
-void oled_write(char c);
-void oled_print(char* str);
+void oled_print(char*);
 void oled_set_line(uint8_t);
+void oled_cursor(uint8_t, uint8_t);
+void oled_plot_char(char);
 
 #endif
--- src/ssd1306/main.c
+++ src/ssd1306/main.c
@@ -5,11 +5,12 @@
 void main(void) {
     init();
     oled_init();
+
     oled_print("* CH552 with OLED   *");
+    oled_cursor(0,1);
     oled_print("---------------------");
-    oled_print("Nicola Strappaazzon\n");
-    oled_print("!@#$%^&*()_-+=<>,.?/\n");
-    oled_print("\n");
+    oled_cursor(0,2);
+    oled_print("Nicola Strappaazzon");
 
     while(1) {}
 }
