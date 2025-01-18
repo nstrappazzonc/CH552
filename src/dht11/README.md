4a81f6d221ce7e300643ba3a93707a2eadd9f506
Nicola Strappazzon <nicola@strappazzon.me>
2025-01-18 01:31:21 +0100

    Save changes at: 1737160281

--- src/dht11/README.md
+++ src/dht11/README.md
@@ -1,3 +1,5 @@
 # DHT11 - Sensor de temperatura y humedad
 
+Este sensor usa un protocolo de comunicación propietario del tipo "single wire".
+
 **NOTA:** Para que la comunicación con el sensor funcione, el reloj debe estar bien sincronizado, asegúrate de verificarlo con un osciloscopio o un analizador lógico.
--- src/dht11/main.c
+++ src/dht11/main.c
@@ -1,137 +1,93 @@
 #include <ch552.h>
-#include <gpio.h>
+// #include <gpio.h>
 #include <utils.h>
 #include <ssd1306.h>
 #include <stdio.h>
 
+SBIT(P32, 0xB0, 2); // P3.2
+
 #define DHT11 P32
 
-// int I_RH, D_RH, I_Temp, D_Temp, CheckSum; 
+uint8_t I_H, D_H, I_T, D_T, CheckSum; 
 
-// #define _nop_() __asm nop __endasm
+uint8_t receive_data(void) {
+    uint8_t byte = 0;
+    for (uint8_t i=0; i<8; i++) {
+        while(!DHT11);
 
-// void delay_us2(uint16_t us) {
-//     while (us--) {
-//         _nop_();
-//     }
-// }
+        delay_us(40);
+        byte <<= 1;
+        byte |= DHT11;
+        // if(DHT11){
+        //     // c <<= 1;
+        //     byte = (byte<<1)|(0x01);
+        // }else{
+        //     // c |= 1;
+        //     byte = (byte<<1);
+        // }
 
-// void delay_ms(uint16_t ms) {
-//     while (ms--) {
-//         delay_us(1000);
-//     }
-// }
-
-// uint8_t request(void) {
-//     PIN_low(DHT11);
-//     delay_ms(20);
-//     PIN_high(DHT11);
-//     // delay_ms(20);
-
-//     // if (DHT11) return 10;  // Error si el pin no baja
-//     // while (!DHT11);
-//     // while (DHT11 == 1);
-
-//     while(DHT11==1);
-//     while(DHT11==0);
-//     while(DHT11==1);
-
-//     return 0;
-// }
-
-// uint8_t DHT11_read_byte(void) {
-//     uint8_t byte = 0;
-//     for(uint8_t i = 0; i < 8; i++) {
-//         while(DHT11==0);  // Espera a que el pin suba a nivel alto
-
-//         delay_us2(30);  // Espera para determinar si es un 0 o un 1
-//         if (DHT11==1) {
-//             byte |= (1 << (7 - i));  // Si el pin sigue en alto, es un 1
-//             // byte = (byte<<1)|(0x01);
-//         } else {
-//             // byte = (byte<<1);
-//         }
-
-//         while(DHT11 == 1);  // Espera a que el pin vuelva a nivel bajo
-//     }
-//     return byte;
-// }
-
-// void response() {
-//     // delay(40);
-//     // PIN_read(DHT11);
-//     // PIN_read(DHT11);
-//     // PIN_read(DHT11);
-
-//     while(PIN_read(DHT11) == 1);
-//     while(PIN_read(DHT11) == 0);
-//     while(PIN_read(DHT11) == 1);
-// }
-
-// int receive_data() {
-//     int q,c=0;  
-//     for (q=0; q<8; q++) {
-//         while(PIN_read(DHT11) == 0);
-
-//         delay(30);
-
-//         if(PIN_read(DHT11) == 1)
-//             // c <<= 1;
-//             c = (c<<1)|(0x01);
-//         else
-//             // c |= 1;
-//             c = (c<<1);
-
-//         while(PIN_read(DHT11) == 1);
-//     }
-//     return c;
-// }
+        // while(!DHT11);
+    }
+    return byte;
+}
 
 void main(void) {
     unsigned char dat[20];
 
     init();
     oled_init();
+    delay(100);
+
+    P3_MOD_OC |= (1 << DHT11);
+    P3_DIR_PU |= (1 << DHT11);
 
     // PIN_high(DHT11);
-    // delay(1000);
+
+    oled_cursor(0,0);
+    oled_print("DHT11");
 
     while(1) {
-        uint8_t data[5] = {0};
+        // PIN_high(DHT11);
+        // delay(10);
 
+        // PIN_low(DHT11);
+        DHT11 = 0;
+        delay(18);
+        DHT11 = 1;
+        // PIN_high(DHT11);
 
-        oled_cursor(0,0);
-        oled_print("Ok1");
-        delay(50);
-        // uint8_t a = request();
-        // response();
-        sprintf(dat, "Ok2");
-        oled_cursor(0,0);
-        oled_print(dat);
+        while(!DHT11);
+        // while(DHT11==1);
+        oled_cursor(0,1);
+        oled_print("-> Ok 1");
+        while(DHT11);
+        // while(DHT11==0);
+        oled_cursor(0,2);
+        oled_print("-> Ok 2");
 
-        // for (uint8_t i = 0; i < 5; i++) {
-        //     data[i] = DHT11_read_byte();
-        // }
+        while(!DHT11);
+        // while(DHT11==1);
+        oled_cursor(0,3);
+        oled_print("-> Ok 3");
 
-        // I_RH    = receive_data(); // store first eight bit in I_RH.
-        // D_RH    = receive_data(); // store next eight bit in D_RH.
-        // I_Temp  = receive_data(); // store next eight bit in I_Temp.
-        // D_Temp  = receive_data(); // store next eight bit in D_Temp.
-        // CheckSum= receive_data(); // store next eight bit in CheckSum.
+        I_H     = receive_data(); // store first eight bit in I_RH.
+        D_H     = receive_data(); // store next eight bit in D_RH.
+        I_T     = receive_data(); // store next eight bit in I_Temp.
+        D_T     = receive_data(); // store next eight bit in D_Temp.
+        CheckSum = receive_data(); // store next eight bit in CheckSum.
 
-        // if ((I_RH + D_RH + I_Temp + D_Temp) != CheckSum) {
-        //     oled_cursor(0,0);
-        //     oled_print("Read DHT11 Error");
-        // }
+        if ((I_H + D_H + I_T + D_T) != CheckSum) { // TODO: If x == 0 is err.
+            oled_cursor(0,4);
+            oled_print("-> Sensor Error.");
+        } else {
+            oled_cursor(0,4);
+            oled_print("-> Sensor Ok.");
+        }
 
-        // sprintf(dat, "H: %d", data[0]);
-        // oled_cursor(0,1);
-        // oled_print(dat);
         // sprintf(dat, "T: %d.%dC", I_Temp, D_Temp);
-        // oled_cursor(0,1);
+        // oled_cursor(0,5);
         // oled_print(dat);
-        PIN_low(DHT11);
-        delay(50);
-        PIN_high(DHT11);
+
+        delay(1000);
     }
 }
