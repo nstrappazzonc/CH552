1622916e445a5a3d62db461412f338957129a342
Nicola Strappazzon <nicola@strappazzon.me>
2025-01-25 13:04:31 +0100

    Save changes at: 1737806671

--- src/dht11/main.c
+++ src/dht11/main.c
@@ -4,21 +4,32 @@
 #include <stdio.h>
 
 SBIT(P32, 0xB0, 2);
-SBIT(P14, 0x90, 4);
-
 #define DHT11 P32
-#define DEBUG1 P14
 
 uint8_t IH, DH, IT, DT, CS;
 
-void DHT11_SetOutput() {
+void DHT11_SetOutput(void) {
     P3_MOD_OC &= ~(1 << 2);  // Configura P3.0 como push-pull
     P3_DIR_PU |= (1 << 2);   // Configura P3.0 como salida
 }
 
-void DHT11_SetInput() {
+void DHT11_SetInput(void) {
     P3_MOD_OC |= (1 << 2);   // Configura P3.0 como open-drain
     P3_DIR_PU &= ~(1 << 2);  // Configura P3.0 como entrada
+}
+
+void DHT11_Start(void) {
+    // Request:
+    DHT11_SetOutput();
+    DHT11 = 0;
+    delay_ms(18);
+    DHT11 = 1;
+
+    // Responce:
+    DHT11_SetInput();
+    while(DHT11 == 1);
+    while(DHT11 == 0);
+    while(DHT11 == 1);
 }
 
 uint8_t DHT11_ReadByte(void) {
@@ -26,9 +37,8 @@ uint8_t DHT11_ReadByte(void) {
 
     for (uint8_t i = 0; i < 8; i++) {
         unsigned int timeout = 10000;
-        DEBUG1 = 1;
+
         while (DHT11 == 0);
-        DEBUG1 = 0;
         delay_us(30);
         if (DHT11 == 1) {
             data |= (1 << (7 - i));
@@ -49,32 +59,17 @@ void main(void) {
     oled_cursor(0,0);
     oled_print("DHT11");
 
-    P1_MOD_OC &= ~(1 << 4);
-    P1_DIR_PU |= (1 << 4);
-
     while(1) {
-        // EA = 0;
-        DHT11_SetOutput();
-        DHT11 = 0;
-        delay_ms(18);
-        DHT11 = 1;
-
-        DHT11_SetInput();
-        while(DHT11 == 1);
-        while(DHT11 == 0);
-        while(DHT11 == 1);
-
+        DHT11_Start();
         IH = DHT11_ReadByte();
         DH = DHT11_ReadByte();
         IT = DHT11_ReadByte();
         DT = DHT11_ReadByte();
         CS = DHT11_ReadByte();
-
-        // EA = 1;
     
         if ((IH + DH + IT + DT) != CS) {
             oled_cursor(0,4);
-            oled_print("-> Sensor Error.");
+            oled_print("-> Sensor read error.");
         }
 
         sprintf(dat, "T: %d.%d", IT, DT);
